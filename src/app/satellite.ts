dfcf76555ea2e9fcd85790313ed16d735affc8e6
darjerpor <djporter94@gmail.com>
2020-08-03 22:05:26 -0500

    Added constructors to Satellite class

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,7 +1,18 @@
+import { stringify } from 'querystring';
+
 export class Satellite {
     name: string;
     orbitType: string;
     type: string;
     operational: boolean;
     launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+        name;
+        type;
+        launchDate;
+        orbitType;
+        operational;
+    }
+
 }
